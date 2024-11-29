798de000982093f2503bd15171df953fb830cc87
Manuel Nuila <manuel.nuila@cuemby.com>
2024-11-28 23:21:25 -0600

    test

--- main.tf
+++ main.tf
@@ -28,30 +28,11 @@ resource "helm_release" "fluent_bit" {
 locals {
   context = var.context
 
-  # Definir los valores predeterminados explícitamente
-  default_config_output = {
-    Name              = "http"
-    Match             = "*"
-    host              = "<victoria-logs-service>.<victoria-logs-service-namespace>.svc.cluster.local"
-    port              = "9428"
-    uri               = "/insert/jsonline?_stream_fields=stream&_msg_field=log&_time_field=date"
-    format            = "json_lines"
-    json_date_format  = "iso8601"
-    compress          = "gzip"
-  }
-
-  # Filtrar claves explícitas
-  config_filtered = {
-    for k, v in var.config_output :
-    k => v if !(contains(keys(local.default_config_output), k) && v == local.default_config_output[k])
-  }
-
-  # Generar la configuración literal
   config_output_literal = <<EOT
 [OUTPUT]
 ${join("\n", [
-  for k, v in local.config_filtered :
-  "    ${k} ${tostring(v)}"
+  for k, v in var.config_output :
+  v != null && v != "" ? "    ${k} ${tostring(v)}" : ""
 ])}
 EOT
 }
